73deb1b73ef1c1c9500fe70bf23281ba70bf118f
kooharim <166689288+koohrim12@users.noreply.github.com>
2024-07-09 10:48:57 +0900

    셋업

---
+++ src/module/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "src\test.css";
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render;
